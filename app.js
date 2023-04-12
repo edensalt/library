1a6059721fefc67d73b27db62ccf5f02236cdf6d
edensalt <124738007+edensalt@users.noreply.github.com>
2023-04-11 22:03:23 -0500

    Add js to iterate through library array and add eslint

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+// Establish the library array
+
+const myLibrary = [];
+
+// Define the object constructor for each book
+
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    //   this.info = function () {
-    //     return `${title} by ${author}, ${pages} pages, ${read}.`;
-    //   };
-  }
-  
-  Book.prototype = {
-    info() {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
-    },
-  };
-  
-  const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-  
-  console.log(theHobbit.info());
-  
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+// Define books
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 309, 'read');
+addBookToLibrary('A Game of Thrones', 'George R.R. Martin', 1088, 'not read yet');
+
+console.log(myLibrary);
+
+for (i = 0; i < myLibrary.length; i++) {
+  const list = document.querySelector('ul');
+  const bookItem = document.createElement('li');
+  bookItem.textContent = `${myLibrary[i].title} by ${myLibrary[i].author} is ${myLibrary[i].pages} pages long. Status? ${myLibrary[i].read}.`;
+  list.appendChild(bookItem);
+}
